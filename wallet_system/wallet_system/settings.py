7176b4b13f0218657d382683cd67c5419a7f0d82
0luv69 <baniyarujal@gmail.com>
2025-08-31 18:49:26 +0545

    changed time to ktm for correct time match

--- wallet_system/wallet_system/settings.py
+++ wallet_system/wallet_system/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party apps
+    'rest_framework',
 
     # Local apps/ The My Appps
     'wallet_api',
@@ -97,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
